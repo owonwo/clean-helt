e434cd9a6fe15105fae995429f0cab5366a48c63
Bruce Lucas <brucelucas@olotusoft.com>
2018-08-06 15:57:59 +0000

    Hospitals, Labs and Pharmacies migrations

---
+++ database/migrations/2018_08_06_134935_create_hospitals_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHospitalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('director_mdcn');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('website')->nullable();
+            $table->integer('facility_type')->nullable();
+            $table->integer('facility_owner')->nullable();
+            $table->string('cac_reg')->nullable();
+            $table->date('cac_date')->nullable();
+            $table->string('fmoh_reg')->nullable();
+            $table->date('fmoh_date')->nullable();
+            $table->string('admin_name')->nullable();
+            $table->string('admin_position')->nullable();
+            $table->string('admin_phone')->nullable();
+            $table->text('services')->nullable();
+            $table->string('bank_name')->nullable();
+            $table->string('bank_branch')->nullable();
+            $table->string('account_name')->nullable();
+            $table->string('account_number')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hospitals');
+    }
+}
---
+++ database/migrations/2018_08_06_151457_create_pharmacies_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePharmaciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pharmacies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('business_name')->nullable();
+            $table->string('business_type')->nullable();
+            $table->integer('facility_owner')->nullable();
+            $table->string('cac_reg')->nullable();
+            $table->date('cac_date')->nullable();
+            $table->string('fmoh_reg')->nullable();
+            $table->date('fmoh_date')->nullable();
+            $table->string('chief_pharmacist_reg');
+            $table->date('chief_pharmacist_reg_date')
+                    ->nullable()->comment('MM/YYYY');
+            $table->string('chief_pharmacist_name')->nullable();
+            $table->string('chief_pharmacist_phone')->nullable();
+            $table->text('services')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pharmacies');
+    }
+}
