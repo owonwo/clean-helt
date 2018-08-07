6d2cd16a402fa3e3c7f035ce307178d751c559dc
SAZZEB <sazzeb@mail.sazzeb.com>
2018-08-07 02:10:23 +0100

    pushed

---
+++ database/factories/LaboratoryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Laboratory::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'licence_no' => str_random(15),
+        'email' => $faker->unique()->safeEmail,
+        'password' => $faker->password(),
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'city' => $faker->city,
+        'state' => $faker->city,
+        'country' => $faker->country,
+        'lab_owner' => $faker->name,
+        'cac_reg' => $faker->str_random(15),
+        'fmoh_reg' => str_random(15),
+        'offers' => $faker->paragraph('3'),
+    ];
+});
---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Patient::class, function (Faker $faker) {
+    $gender = $faker->randomElements(['male', 'female']);
+    $religion = $faker->randomElements(['Christianity', 'Muslim', 'Witch']);
+    $marital_status = $faker->randomElements(['single', 'married', 'engage', 'divorce', 'stile_s']);
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'middle_name' => $faker->lastName,
+        'avatar' => $faker->image(),
+        'email' => $faker->unique()->safeEmail,
+        'dob' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'gender' => $gender,
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'city' => $faker->city,
+        'state' => $faker->city,
+        'country' => $faker->country,
+        'religion' => $religion,
+        'marital_status' => $marital_status,
+
+        'nok_name' => $faker->name,
+        'nok_email' => $faker->unique()->safeEmail,
+        'nok_phone' => $faker->phoneNumber,
+        'nok_address' => $faker->address,
+        'nok_city' => $faker->city,
+        'nok_state' => $faker->city,
+        ];
+});
