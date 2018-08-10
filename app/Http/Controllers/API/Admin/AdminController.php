dfaeba5d1be675a7c193203c4cb398f7002b4b06
mobrad1 <yarrowbradley@gmail.com>
2018-08-10 09:14:04 +0100

    Admin can now create an admin

---
+++ app/Http/Controllers/API/Admin/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    //
+    // Shows the list of all admins with their roles
+    public function index(){
+        $admin = Admin::all();
+        return $admin;
+    }
+    public function store(){
+        $admin =  Admin::forceCreate(request()->all());
+
+            return request()->json([
+                'admin' => $admin,
+                'message' => 'Admin created Successfully'
+            ]);
+
+    }
+    public function show(Admin $admin){
+        return response()->json([
+            'admin' => $admin
+        ]);
+    }
+    public function update(Admin $admin){
+        $admin->update(request()->all());
+        return response()->json([
+            'message' => 'Admin updated successfully',
+            'admin' => $admin
+        ]);
+    }
+}
