55e917dfe8e365ed862ffcf7c24499e330775254
Chiemela Chinedum <chiemelachinedum@gmail.com>
2018-12-02 21:34:51 +0100

    Removes updateRule from Patient/PatientController

--- app/Http/Controllers/API/Patient/PatientController.php
+++ app/Http/Controllers/API/Patient/PatientController.php
@@ -159,20 +159,6 @@ class PatientController extends Controller
      */
     public function update(Request $request)
     {
-        /**
-         * @check if the patient is inserting an image of not
-         */
-        $rule = $this->getUpdateRule();
-
-        try {
-            $this->validate($request, $rule);
-        } catch (ValidationException $exception) {
-            return response()->json([
-                'errors' => $exception->errors(),
-                'message' => $exception->getMessage(),
-            ], 422);
-        }
-
         try {
             $data = request()->all();
             unset($data['avatar']);
@@ -283,18 +269,6 @@ class PatientController extends Controller
             'last_name' => 'required|string|max:60|min:2',
             'password' => 'required|max:32|min:6',
             'phone' => 'required|digit:11',
-        ];
-    }
-
-    private function getUpdateRule()
-    {
-        return [
-            'first_name' => 'required|string|max:60|min:2',
-            'last_name' => 'required|string|max:60|min:2',
-            'address' => 'required',
-            'city' => 'required',
-            'state' => 'required',
-            'country' => 'required',
         ];
     }
 
