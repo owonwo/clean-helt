0e776ed1546ead8d5c6818e4ccacc45474c9c1e9
Bruce Lucas <brucelucas@olotusoft.com>
2019-01-12 13:10:56 +0000

    Adds seeders for hospital contact and medical history

---
+++ database/seeds/MedicalHistorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\MedicalHistory;
+
+class MedicalHistorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        MedicalHistory::truncate();
+        
+        $patients = factory("App\Models\Patient", 10)->create();
+
+        $patients->each(function($patient) {
+            $record = factory("App\Models\MedicalRecord")->create(['patient_id' => $patient->id]);
+            factory("App\Models\MedicalHistory")->create(['record_id' => $record->id]);
+        });
+    
+    }
+}
